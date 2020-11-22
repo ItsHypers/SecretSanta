8ab99444366dd75961d6d5f95411b1490c44acc9
ItsHypers <59265736+ItsHypers@users.noreply.github.com>
2020-11-22 05:10:06 +0000

    Sprites being cunts
    fixed camera following player sprits; making player ones, fixed sprite tearing bug

---
+++ Secret Santa/Assets/Script/CameraFollow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform FollowTarget;
+	
+
+    void Update() {
+        transform.position = new Vector3(FollowTarget.transform.position.x + 0.8f,FollowTarget.transform.position.y,FollowTarget.transform.position.z - 8);
+    }
+
+}
